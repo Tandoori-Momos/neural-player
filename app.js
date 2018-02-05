323e79ee6149e350e1f2337595a2efa2fcc5d245
Tandoori Momos <yash20008@gmail.com>
2018-02-06 00:35:01 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,35 @@
+
+const express = require('express'); // Add express
+const app = express(); // Store express functions in app
+
+const bodyParser = require('body-parser');
+
+const ejs = require('ejs'); // View engine
+
+const songs = require('./array.js').Array; // Get Songs Array
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.set('view engine', 'ejs'); // Folder for views
+app.use(express.static('public')); // Default Folder for assets
+
+// Port for running app
+app.listen('8080', function() {
+  console.log('listening to port 8080');
+});
+
+app.get('/', function(req,res) {
+  res.render('main.ejs'); // Webpage displayed at '/'
+});
+
+app.get('/train', function(req,res) {
+  res.render('neural.ejs'); // Webpage displayed at '/train'
+});
+
+// If the user clicks play button
+app.post('/play', urlencodedParser, function (req, res) {
+  var _song = songs[Math.floor(Math.random()*songs.length)]; // Random Song
+  var resp = JSON.stringify(_song);
+  if (!req.body) return res.sendStatus(400)
+  res.send(_song);
+})
---
+++ array.js
@@ -0,0 +1,24 @@
+
+module.exports.Array =  [
+  {
+      'title': 'Homeward',
+      'artist': 'The Sundays',
+      'id': 'jgc0oTynu5A',
+      'tags': ['dream pop', 'female singer', 'slow', 'indie']
+  },
+  {
+      'title': 'Life Goes On',
+      'artist': 'The Sundays',
+      'id': 'nNW0x3qAbtw',
+      'tags': ['dream pop', 'female singer', 'slow', 'indie']
+  },
+  {
+      'title': 'Heres Where The Story Ends',
+      'artist': 'The Sundays',
+      'id': 'Yq9x-ff0fXs',
+      'tags': ['dream pop', 'female singer', 'slow', 'indie']
+  }
+
+
+
+]; ;
